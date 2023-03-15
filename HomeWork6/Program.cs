d9eeecf1cf3354020a094343dca820d02f32b34b
Konstantin Os <permkonstantin@gmail.com>
2023-03-15 22:19:32 +0500

    hw6_ready

--- HomeWork6/Program.cs
+++ HomeWork6/Program.cs
@@ -1,48 +1,28 @@
-﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-double Prompt(string userMessage)
+int Prompt(string userMessage)
 {
     Console.Write(userMessage);
-    double result = Convert.ToInt32(Console.ReadLine());
+    int result = Convert.ToInt32(Console.ReadLine());
     return result;
 }
 
-bool QuestionToUser()
+int[] FillArray(int sizeOfArray)
 {
-    Console.WriteLine("Do you want input another number?");
-    string question = Console.ReadLine();
+    int[] array = new int[sizeOfArray];
 
-    if (question == "yes".ToLower() || question == "y".ToLower())
+    for (int i = 0; i < array.Length; i++)
     {
-        return true;
+        int num = Prompt($"Input a number {i + 1}: ");
+        array[i] = num;
     }
-    else
-    {
-        return false;
-    }
+
+    return array;
 }
 
-double[] FillArray(double number)
-{
-    int quantityArrEl = 1;
-    double[] numbers = new double[quantityArrEl];
-
-    numbers[0] = number;
-
-    if (QuestionToUser())
-    {
-        quantityArrEl++;
-        number = Prompt("Input a number: ");
-        numbers[quantityArrEl + 1] = number;
-        QuestionToUser();
-    }
-
-    return numbers;
-}
-
-int greaterThanZero(double[] someArr)
+int GreaterThanZero(int[] someArr)
 {
     int count = 0;
 
@@ -56,11 +36,47 @@ int greaterThanZero(double[] someArr)
     return count;
 }
 
-double number = Prompt("Input a number: ");
+int sizeOfArray = Prompt("Input a quantity of number: ");
 
-double[] numbers = FillArray(number);
-int result = greaterThanZero(numbers);
+int[] numbers = FillArray(sizeOfArray);
+int result = GreaterThanZero(numbers);
 
 Console.WriteLine($"Input numbers: [{string.Join(", ", numbers)}]");
 Console.Write($"Quantity numbers greater than zero: {result}");
+*/
 
+/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double Prompt(string userMessage)
+{
+    Console.Write(userMessage);
+    double result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+double GetPointX(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+
+    return x;
+}
+
+double GetPointY(double b1, double k1, double b2, double k2, double x)
+{
+    double y = (k1 * x + b1);
+
+    return y;
+}
+
+double b1 = Prompt("Input b1: ");
+double k1 = Prompt("Input k1: ");
+double b2 = Prompt("Input b2: ");
+double k2 = Prompt("Input k2: ");
+double pointX = GetPointX(b1, k1, b2, k2);
+
+double pointY = GetPointY(b1, k1, b2, k2, pointX);
+
+Console.Write($"{pointX}; {pointY}");
+*/
